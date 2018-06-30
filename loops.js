0b378c4e362002f78aaed9db74ecf442f2000e79
Peter Traversa <peter.traversa@gmail.com>
2018-06-30 22:22:13 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -18,5 +18,11 @@ function whileLoop(number) {
 }
 
 function doWhileLoop(array) {
-  
+  function maybeTrue() {
+    return Math.random() >= 0.5;
+  }
+  do {
+    array.pop();
+  } while (array.length > -1 && maybeTrue());
+  return array;
 }
